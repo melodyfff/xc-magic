2a0345999080c86dd3b38599238a05a85c433925
melodyfff <307208327@qq.com>
2019-03-11 23:50:55 +0800

    :smile: Enable AspectJ Auto Proxy

--- base/src/main/java/com/xinchen/base/config/AppConfig.java
+++ base/src/main/java/com/xinchen/base/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.xinchen.base.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 
@@ -9,11 +10,13 @@ import org.springframework.stereotype.Controller;
  *
  * Spring环境配置
  * 排除web相关，避免被重复扫描
+ * EnableAspectJAutoProxy 开启aop自动扫描注解  <aop:aspectj-autoproxy />
  *
  * @author Xin Chen (xinchenmelody@gmail.com)
  * @version 1.0
  * @date Created In 2019/2/28 23:26
  */
+@EnableAspectJAutoProxy
 @Configuration
 @ComponentScan(basePackages = "com.xinchen.base",
         excludeFilters = {
