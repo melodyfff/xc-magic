0f0171ce40bee4aec918a90095195cd3113adc02
melodyfff <307208327@qq.com>
2019-09-22 14:04:29 +0800

    :zap: spring data access

---
+++ spring/data/src/main/java/com/xinchen/spring/data/App.java
@@ -0,0 +1,10 @@
+package com.xinchen.spring.data;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
